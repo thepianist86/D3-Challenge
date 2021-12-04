eef06828a9a461633e8d18e731eb27233f2ceda3
Timothy Kelley <thepianist86@gmail.com>
2021-12-04 17:18:17 -0600

    visual changes

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,6 @@
-// @TODO: YOUR CODE HERE!
-// Significantly coded live along with Nathan during group homework help session
+// Significantly coded live along with Nathan D. during group homework help session
 var svgSize = {
-    width:800,
+    width: 800,
     height: 450
 }
 
@@ -13,21 +12,21 @@ var margin = {
 }
 
 var chartSize = {
-    width: svgSize.width-margin.left-margin.right,
-    height: svgSize.height-margin.top-margin.bottom
+    width: svgSize.width - margin.left - margin.right,
+    height: svgSize.height - margin.top - margin.bottom
 }
 
-var radius = 15
+var radius = 10;
 
 var svg = d3.select("#scatter").append("svg")
-                                .attr("width", svgSize.width)
-                                .attr("height", svgSize.height)
-                                .attr("style", "border:1px solid black")
+    .attr("width", svgSize.width)
+    .attr("height", svgSize.height)
+    .attr("style", "border:1px solid black")
 var frame = svg.append('g')
-                .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-d3.csv(".\\assets\\data\\data.csv").then(data =>{
-    console.log(data)
+d3.csv(".\\assets\\data\\data.csv").then(data => {
+    //console.log(data)
     data.forEach(state => {
         state.healthcare = +state.healthcare;
         state.income = +state.income
@@ -37,50 +36,46 @@ d3.csv(".\\assets\\data\\data.csv").then(data =>{
     var yvals = data.map(state => state.income)
 
     var xScale = d3.scaleLinear()
-                    .domain([.9 * d3.min(xvals), d3.max(xvals)])
-                    .range([0, chartSize.width])
+        .domain([.9 * d3.min(xvals), d3.max(xvals)])
+        .range([0, chartSize.width])
     var yScale = d3.scaleLinear()
-                    .domain(.9 * [d3.min(yvals), d3.max(yvals)])
-                    .range([chartSize.height, 0])
-    
+        .domain([.9 * d3.min(yvals), d3.max(yvals)])
+        .range([chartSize.height, 0])
+
     var bottomAxis = d3.axisBottom(xScale)
     var leftAxis = d3.axisLeft(yScale)
 
     frame.append("g").attr("transform", `translate(0, ${chartSize.height})`)
-                    .call(bottomAxis)
+        .call(bottomAxis)
     frame.append("g").call(leftAxis)
 
     var chartData = frame.append("g").attr("id", "ChartData")
 
     var toolTip = d3.tip()
-                    .attr("class", "tooltip")
-                    .offset([80,60])
-                    .html(function(d){
-                        return ("<div>Hi</div>")
-                    })
-    chartData.call(toolTip)
+        .attr("class", "d3-tip")
+        .offset([-8, 0])
+        .html(function (d) {
+            return "State: " + d.state + "<br>Healthcare: " + d.healthcare +"%" + "<br>Income: $" + d.income;
+        })
+    chartData.call(toolTip);
+
 
     var circles = chartData.selectAll("circle").data(data)
-                            .enter().append("circle")
-                            .on('mouseover', function(d){
-                                toolTip.show(data, this);
-                            })
-                            .on('mouseout', function(d){
-                                toolTip.hide(data, this);
-                            })
-                            .attr("r", radius)
-                            .attr("cx", state => xScale(state.healthcare))
-                            .attr("cy", state => yScale(state.income))
-                            .attr('stroke', 'blue')
-                            .attr('fill', "white")
-                            .attr("opacity", ".5")
-                            
+        .enter().append("circle")
+        .attr("r", radius)
+        .attr("cx", state => xScale(state.healthcare))
+        .attr("cy", state => yScale(state.income))
+        .attr('stroke', 'blue')
+        .attr('fill', "blue")
+        .attr("opacity", ".5")
+        .on('mouseover', toolTip.show)
+        .on('mouseout', toolTip.hide)
+
 
     var text = chartData.selectAll("text").data(data)
-                                .enter().append("text")
-                                .attr("dx", state => xScale(state.healthcare) - radius/2)
-                                .attr("dy", state => yScale(state.income) + radius/2)
-                                .text(state => state.abbr)
+        .enter().append("text").attr("class", "stateText")
+        .attr("dx", state => xScale(state.healthcare) - radius / 2)
+        .attr("dy", state => yScale(state.income) + radius / 2)
+        .text(state => state.abbr)
 })
 
-                
